cb2ee3d8b6a1b6d7d1820dbe5019233e99b82ba5
ziyang.liu <ziyang.liu@zhangmen.com>
2020-06-26 01:37:24 +0800

    add hook

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = new express();
+const bodyParser = require("body-parser");
+
+// parser application/json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is my blog");
+});
+
+// 设置
+app.post("/payload", (req, res) => {
+  console.log("req.body", req.body);
+  res.send(`I got some json: ${req.body.inspect}`);
+});
 
 const port = 1024;
-
-app.get('/', (req, res) => {
-  res.send("Hello, this is my blog");
-})
-
 app.listen(port, () => {
-  console.log(`app is runing in http://localhost:${port}`)
-})
+  console.log(`app is runing in http://localhost:${port}`);
+});
