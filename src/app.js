861d09a0d8e1e8747d43eea69d0539affe460561
ziyang.liu <ziyang.liu@zhangmen.com>
2020-06-26 13:33:14 +0800

    add hook test

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const githubWebhook = require('./middleware/githubWebhook');
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is my blog test webhook 6");
+  res.send("Hello, this is my blog test webhook 7");
 });
 
 // 设置
