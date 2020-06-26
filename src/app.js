847ac0dc1bd0f7f391a932530753450b46261189
ziyang.liu <ziyang.liu@zhangmen.com>
2020-06-26 13:11:21 +0800

    add hook test

--- src/app.js
+++ src/app.js
@@ -2,23 +2,25 @@ const express = require("express");
 const app = new express();
 const bodyParser = require("body-parser");
 const child_process = require('child_process');
+const githubWebhook = require('./middleware/githubWebhook');
+require('dotenv').config()
 
 // parser application/json
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is my blog test webhook 3");
+  res.send("Hello, this is my blog test webhook");
 });
 
 // 设置
-app.post("/payload", (req, res) => {
+app.post("/payload", githubWebhook, (req, res) => {
   console.log("req.body", req.body);
-  child_process.exec('bash ./deploy.sh', {cwd: "/data/express-blog"}, (error, stdout, stderr) => {
+  child_process.exec('bash deploy.sh', {cwd: "/data/express-blog"}, (error, stdout, stderr) => {
     if(error || stderr) {
       res.send(`I got error:${JSON.stringify(error)}, stderr:${JSON.stringify(stderr)}`);
     } else {
       // ${req.body.inspect}
-      res.send(`I got some json: ${stdout}`);
+      res.send(`I got some json`);
     }
 
   })
